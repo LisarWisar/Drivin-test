299cee2c29135b4ce698bff55702c67fcb0858fb
LisarWisar <vsoto.zoro@gmail.com>
2024-07-31 22:15:16 -0400

    Added table with asc/desc ordering using made up data of 40 entries

---
+++ app/lib/utils.tsx
@@ -0,0 +1,19 @@
+
+export default function TestData () {
+    let newData = [];
+    for (let i = 0; i < 40; i++){
+        newData.push({
+            "id": i+1, //API has no id
+            "class": "clase",
+            "fuel_type": "combustible",
+            "make": "Toyota",
+            "model": "Camry",
+            "year": i+1,
+            "transmission": "manual",
+            "city_mpg": "40 mpg",
+            "highway_mpg": "30 mpg",
+            "combination_mpg": "20 mpg"
+        })
+    }
+    return newData;
+}
---
+++ app/ui/vehicles-table/table.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import React from "react";
+import { useState } from "react";
+
+import TestData from "@/app/lib/utils"
+
+
+export default function table () {
+
+    const [sortColumn, setSortColumn] = useState(null);
+    const [sortOrder, setSortOrder] = useState("asc");
+
+    let testData = TestData(); //test data of 40 entries
+
+    const sortedProducts = [...testData].sort((a, b) => {
+        if (sortColumn) {
+            if (sortOrder === "asc") {
+            return a[sortColumn] < b[sortColumn] ? -1 : 1;
+            } else {
+            return a[sortColumn] > b[sortColumn] ? -1 : 1;
+            }
+        } else {
+            return 0;
+        }
+    });
+
+    const handleSort = (column) => {
+        if (sortColumn === column) {
+          setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+        } else {
+          setSortColumn(column);
+          setSortOrder("asc");
+        }
+      };
+    
+
+    return(
+        <table className="min-w-full">
+            <thead>
+                <tr>
+                    <th scope="col" onClick={() => handleSort("class")}>
+                        Tipo de auto
+                    </th>
+                    <th scope="col" onClick={() => handleSort("fuel_type")}>    
+                        Tipo de combustible
+                    </th> 
+                    <th scope="col" onClick={() => handleSort("make")}>
+                        Marca
+                    </th> 
+                    <th scope="col" onClick={() => handleSort("model")}>
+                        Modelo
+                    </th> 
+                    <th scope="col" onClick={() => handleSort("year")}>
+                        Año
+                    </th> 
+                    <th scope="col" onClick={() => handleSort("transmission")}>
+                        Tipo de transmisión
+                    </th>
+                    <th scope="col" onClick={() => handleSort("city_mpg")}>
+                        Consumo en ciudad
+                    </th>  
+                    <th scope="col" onClick={() => handleSort("highway_mpg")}>
+                        Consumo en carretera
+                    </th>  
+                    <th scope="col" onClick={() => handleSort("combination_mpg")}>
+                        Consumo mixto
+                    </th>  
+                </tr>
+            </thead>
+            <tbody>
+                {sortedProducts.map((car) => ( //Caution, API response has no id column, must be added manually
+                    <tr key={car.id}>
+                        <td>{car.class}</td>
+                        <td>{car.fuel_type}</td>
+                        <td>{car.make}</td>
+                        <td>{car.model}</td>
+                        <td>{car.year}</td>
+                        <td>{car.transmission}</td>
+                        <td>{car.city_mpg}</td>
+                        <td>{car.highway_mpg}</td>
+                        <td>{car.combination_mpg}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
+}
